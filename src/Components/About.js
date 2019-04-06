d716d73b12fec0e627a2766126a3b6e0d625740e
jamesamrundle <jamesamrundle@gmail.com>
2019-04-06 16:07:58 -0400

    finish abt component

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,22 +5,47 @@ class About extends Component {
        super(props)
 
        this.state= {me:{name: "James Rundle",
-               bio:"Hi I am James I. I like eating bread. I like writing code. I like making money. Hire me."},
-
+               bio:"Hi I am James I. I like eating bread. I like writing code. I like making money. Hire me.",
+           phone:"816-777-8998",
+           email:"jamesamrundle@gmail.com"},
+                address:{street: "666 Gumdrop Lane",
+                        city: "Charleston",
+                        state: "South Carolina",
+                        zip:"29411"}
                     }
        }
 
 
     render() {
-        let {state} = this;
+        let {me,address} = this.state;
         return (
 
 
             <section id={"about"}>
                 <div>
+                    <div className={"pic"}>
+                        <img className={"profile-pic"} src={require("../images/hiamjamz.jpg")} alt={"my handsome visage"}/>
+                    </div>
+
                     <div className={"main-col"}>
-                        <h4>About Me</h4>
-                        <p>{this.state.me.bio}</p>
+                        <h3>About Me</h3>
+                        <p>{me.bio}</p>
+
+                        <div className={"contact-details"}>
+                            <h3>Contact Details</h3>
+                            <p>
+                                <span>{me.name}</span> <br/>
+                                <span>{address.street}</span> <br/>
+                                <span>{address.city}, {address.state} {address.zip}</span> <br/>
+                            </p>
+                            <a style={{ float:"right"}} className={"button"} href={process.env.PUBLIC_URL+"/resume.pdf"} >Download Resume</a>
+
+                            <p>
+                                <span>{me.phone}</span> <br/>
+                                <span>{me.email}</span> <br />
+                            </p>
+
+                        </div>
                     </div>
                 </div>
 
